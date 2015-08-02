1d25984e757535968e9155e0fb16888a2483c54c
Joel Coulson <joel@joelcoulson.com>
2015-08-02 16:54:57 +0800

    Initial Commit

---
+++ src/Datatypes.py
@@ -0,0 +1,11 @@
+# integer
+a = int(43)
+print(type(a), a)
+
+# float
+b = float(43.1)
+print(type(b), b)
+
+# string
+c = "This is a string"
+print(type(c), c)
---
+++ src/Variables.py
@@ -0,0 +1,8 @@
+x = 3
+y = 5
+
+# note the int to string casting
+print(str(x) + "+" + str(y) + "=" + str(x+y))
+
+# showing the type of the variable x
+print("The variable x is an " + str(type(x)))
