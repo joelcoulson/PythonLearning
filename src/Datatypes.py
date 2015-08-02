97fff05d993807de4fa6a2270b68e7ba213e62b1
Joel Coulson <joel@joelcoulson.com>
2015-08-02 21:51:39 +0800

    Commit

--- src/Datatypes.py
+++ src/Datatypes.py
@@ -1,11 +1,29 @@
+# boolean
+myBool = True
+
 # integer
-a = int(43)
-print(type(a), a)
+myInt = int(43)
+print(type(myInt), myInt)
 
 # float
-b = float(43.1)
-print(type(b), b)
+myFloat = float(43.1)
+print(type(myFloat), myFloat)
 
 # string
-c = "This is a string"
-print(type(c), c)
+myNum = 53
+myString = "This is " + str(myNum) + " a string"
+print(type(myString), myString)
+
+# tuples
+myTuple = (1,2,3,4,5)
+print(myTuple)
+
+# lists
+myList = [1,2,3,4,5]
+myList.append(6)
+print(myList)
+print(myList[1]) # printing the second element
+
+# dictionaries (otherwise known as hashes in other languages)
+myDict = dict(one=1, two=2)
+print(myDict['one'])
